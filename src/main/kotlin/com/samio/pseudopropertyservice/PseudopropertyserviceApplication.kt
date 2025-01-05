c949b9dd9b102052094c0132f49a58fb33c48171
aron <vaupelaron@gmail.com>
2025-01-05 17:45:33 +0700

    Update component scanning and logging configurations
    Modified `ComponentScan` and `EnableJpaRepositories` to exclude specific packages using regex filters for better modularity. Added DEBUG-level logging for Spring contexts in `application.yml` to enhance traceability during debugging.

--- src/main/kotlin/com/samio/pseudopropertyservice/PseudopropertyserviceApplication.kt
+++ src/main/kotlin/com/samio/pseudopropertyservice/PseudopropertyserviceApplication.kt
@@ -3,12 +3,27 @@ package com.samio.pseudopropertyservice
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.FilterType
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.scheduling.annotation.EnableScheduling
 
-@SpringBootApplication(scanBasePackages = ["com.samio.pseudopropertyservice", "com.ecommercedemo.common"])
+@SpringBootApplication
+@ComponentScan(
+	basePackages = ["com.samio.pseudopropertyservice", "com.ecommercedemo.common"],
+	excludeFilters = [ComponentScan.Filter(
+		type = FilterType.REGEX,
+		pattern = ["com\\.ecommercedemo\\.common\\.model\\.concretion\\._pseudoProperty\\..*"]
+	)]
+)
 @EntityScan("com.samio.pseudopropertyservice.model")
-@EnableJpaRepositories("com.samio.pseudopropertyservice.persistence")
+@EnableJpaRepositories(
+	basePackages = ["com.samio.pseudopropertyservice.persistence"],
+	excludeFilters = [ComponentScan.Filter(
+		type = FilterType.REGEX,
+		pattern = ["com\\.ecommercedemo\\.common\\.persistence\\..*"]
+	)]
+)
 @EnableScheduling
 class PseudopropertyserviceApplication
 
