22fa48e408901d8c2379b39b03effc8c5ea9f1c4
aron <vaupelaron@gmail.com>
2025-01-05 19:02:05 +0700

    Remove unused import and downgrade Spring Boot version
    Removed the `ConditionalOnClass` import as it was unused in `PseudoPropertyController`. Downgraded the Spring Boot version from 3.4.1 to 3.3.4 in the `build.gradle.kts` file to maintain compatibility or address potential issues.

--- src/main/kotlin/com/samio/pseudopropertyservice/controller/PseudoPropertyController.kt
+++ src/main/kotlin/com/samio/pseudopropertyservice/controller/PseudoPropertyController.kt
@@ -3,7 +3,6 @@ package com.samio.pseudopropertyservice.controller
 import com.ecommercedemo.common.controller.abstraction.RestControllerTemplate
 import com.ecommercedemo.common.controller.annotation.ControllerFor
 import com.samio.pseudopropertyservice.model.PseudoProperty
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
