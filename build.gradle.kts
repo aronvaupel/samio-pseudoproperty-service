acd3583a89a949313b54428003915c1f34db6210
aron <vaupelaron@gmail.com>
2025-01-08 04:13:17 +0700

    Update commons library to version 6.6.12
    Upgraded the dependency to address potential bug fixes or improvements in the commons library. Ensures the project uses the latest stable version for enhanced stability and performance.

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,7 +42,7 @@ extra["springCloudVersion"] = "2023.0.3"
 val isLocalProfile: Boolean = project.hasProperty("spring.profiles.active") && project.property("spring.profiles.active") == "local"
 
 dependencies {
-	implementation("com.github.aronvaupel:commons:6.6.11")
+	implementation("com.github.aronvaupel:commons:6.6.12")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
